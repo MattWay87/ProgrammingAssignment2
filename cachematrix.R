da82220c644de585e9b7b3a6cce671fad76d07c3
Matthew Way <34617206+MattWay87@users.noreply.github.com>
2018-11-19 22:00:41 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author: Matt Way
+## Date: November 18, 2018
 
-## Write a short comment describing this function
+## These functions will allow the user to create a special object
+## which will store a matrix input by a user and cache its inverse.
+## If the user requests the inverse of a matrix that has already been cached
+## the function will return the cached value.
 
-makeCacheMatrix <- function(x = matrix()) {
 
+## The makeCacheMatrix function will create the a list of functions
+## based on the matrix input by the user which can be called from the cache
+## by other functions. The list of functions created by makeCacheMatrix includes
+## a function to set the value of the matrix, return the matrix, set the inverse
+## value, and return the inverse value.
+
+makeCacheMatrix <- function(a = matrix()) {
+      b <- NULL
+      set <- function(c) {
+            a <<- c
+            b <<- NULL
+      }
+      get <- function() a
+      setinverse <- function(solve) b <<- solve
+      getinverse <- function() b
+      list(set = set,get = get,setinverse = setinverse,getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function will allow the user to input the special object
+## created by the makeCacheMatrix function. If the inverse of that matrix has
+## been cached, that value will be returned, along with a message indicating
+## this. Otherwise, the inverse of the matrix will be calculated and returned.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(d, ...) {
+      e <- d$getinverse()
+      if(!is.null(e)) {
+            message("getting cached data")
+            return(e)
+      }
+      data <- d$get()
+      e <- solve(data, ...)
+      d$setinverse(e)
+      e
 }
